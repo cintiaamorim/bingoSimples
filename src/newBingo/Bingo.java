2a20cc318ab893695273120213eeea0da34c8066
Cintia Amorim <cintia.amorim@outlook.com>
2022-07-01 23:05:12 -0300

    Nova implementacao do bingo

--- src/newBingo/Bingo.java
+++ src/newBingo/Bingo.java
@@ -1,28 +1,36 @@
 package newBingo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class Bingo {
 	
-	Integer numeroSorteado;
+	Integer numeroSorteado;	
+	
+	String letra;
 	
 	List<Integer> lstBingo = new ArrayList<Integer>();
 	
+	public void verificarLetraDigitada(String letra) {
+		System.out.println("Digite a letra (R para sortear, X para sair)");
+		this.letra = letra;	
+	}
+	
 	public void sortearNumero() {
 		Random aleatorio  = new Random();
-		numeroSorteado = aleatorio .nextInt(99) + 1;
+		numeroSorteado = aleatorio.nextInt(99) + 1;
 		if(lstBingo.isEmpty()) {
 			adicionarNumeroLista(numeroSorteado);
 		} else 
 		verificarNumeroRepetido(numeroSorteado);
 	}
 	
-	public void verificarNumeroRepetido(Integer numeroSorteado) {
+	public void verificarNumeroRepetido(int numeroSorteado) {
 		if(!lstBingo.isEmpty()) {
 		for(int i=0; i < lstBingo.size(); i++) {
-			if(lstBingo.equals(numeroSorteado)) {
+			if(lstBingo.contains(numeroSorteado)) {
 				sortearNumero();
 			} else {
 				adicionarNumeroLista(numeroSorteado);
@@ -39,10 +47,12 @@ public class Bingo {
 
 	
 	public void exibirTodosNumerosSorteados() {
+		System.out.println("Fim do Sorteio");
 		if(lstBingo.isEmpty()) {
 			System.out.println("Nenhum número foi sorteado");
 		} else if(!lstBingo.isEmpty()) {
 		System.out.println("Foram sorteados " + lstBingo.size() + " números");
+		Collections.sort(lstBingo);
 		System.out.println("Os números sorteados são: " + lstBingo.toString());
 		}
 		}
--- src/newBingo/Sorteio.java
+++ src/newBingo/Sorteio.java
@@ -1,5 +1,6 @@
 package newBingo;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Sorteio {
@@ -7,20 +8,17 @@ public class Sorteio {
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
-
-		Bingo bingo = new Bingo();		
 		
 		String letra;
+
+		Bingo bingo = new Bingo();	
 		
 		System.out.println("Digite a letra (R para sortear, X para sair)");
-		letra = entrada.next();
-		if(letra.equalsIgnoreCase("X")) {
-			System.out.println("Fim do sorteio");
-		}
+		letra = entrada.next();		
 		
 		while(letra.equalsIgnoreCase("R")) {
 			bingo.sortearNumero(); 
-			System.out.println("Digite a letra (R para sortear, X para sair)");
+			bingo.verificarLetraDigitada(letra);
 			letra = entrada.next();
 			if(letra.equalsIgnoreCase("X")) {				
 				break;
